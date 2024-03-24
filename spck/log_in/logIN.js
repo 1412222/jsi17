85ba40f80130711cab63cdc7483d50c301afeb4a
quang anh <quanggensimp@gmail.com>
2024-03-24 09:56:56 +0700

    log in

--- spck/log_in/logIN.js
+++ spck/log_in/logIN.js
@@ -1,26 +1,25 @@
 
-  function validateForm() {
+ 
+
     const username = document.getElementById('username');
     const password = document.getElementById('password');
 
-   
+    
     if (username.value === "") {
       alert("Please enter your username or email address.");
       username.focus(); 
-      return false; 
+      return false;
     }
 
-    
+ 
     if (password.value === "") {
       alert("Please enter your password.");
       password.focus();
       return false;
     }
 
-   
+    
     return true;
-  }
+  
 
 
-<form action="/login" method="post" onsubmit="return validateForm()">
-  </form>
